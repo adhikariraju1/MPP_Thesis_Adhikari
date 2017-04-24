772807e8e3ba035ad57743af3634ba9b2509f477
adhikariraju1 <adhikariraju1@gmail.com>
2017-04-24 13:38:44 +0200

    After meeting with Dev

--- source.R
+++ source.R
@@ -30,71 +30,36 @@ source("Statistics_Part2.R")
 descrp_p1 <- merged_df4 %>%
   select(rep.share, repshare.lag, unemp_gro, Pop_thou, white.percent, rep_incumb, rural_percent)
 
-set.seed(42)
+corplot1 <- descrp_p1 %>%
+  select(rep.share, repshare.lag, unemp_gro) %>%
+  ggpairs(lower = list("continuous" = "smooth"))
+#####
+corplot2 <- descrp_p1 %>%
+  select(rep.share, Pop_thou, white.percent, rural_percent) %>%
+  ggpairs(lower = list("continuous" = "smooth"))
 
-correlation_p1 <- data.frame(rep.share = rnorm(100),
-                             repshare.lag = rnorm(100),
-                             unemp_gro = rnorm(100))
-corplot1 <- ggpairs(correlation_p1)
 #####
-correlation_p2 <- data.frame(rep.share = rnorm(100),
-                             Pop_thou = rnorm(100),
-                             white.percent = rnorm(100),
-                             rep_incumb = rnorm(100),
-                             rural_percent = rnorm(100))
-corplot2 <- ggpairs(correlation_p2)
-#####
+
+merged_df4 <- merged_df4 %>%
+  filter(Pop_thou <= 7500)
+
 f1 <- plm(rep.share ~ unemp_gro + repshare.lag, merged_df4, model = 'within')
 f2 <- plm(rep.share ~ unemp_gro + repshare.lag + Pop_thou + white.percent + as.factor(rep_incumb)
           + unemp_gro:as.factor(rep_incumb) + as.factor(rural)
           + white.percent:as.factor(rural), merged_df4, model = 'within')
 
-test1 <- coeftest(f2, vcovHC(f2, method = "arellano"))
+test1 <- coeftest(f1, vcovHC(f1, method = "arellano"))
+test2 <- coeftest(f2, vcovHC(f2, method = "arellano"))
 #####
 #interplot marginal effects plots
 
 #**************************************************************************************************************************
 #For forecasting:
-part1_df <- merged_df4 %>%
-  mutate(pci_gro_sq = PCI_gro^2) %>%
-  select(county.fips, year, county, state, rep.share, repshare.lag, unemp_gro, 
-         PCI_gro, pci_gro_sq, Pop_thou, white.percent, rep_incumb, rural_percent) %>%
-  rename(pop_thou = Pop_thou, pci_gro = PCI_gro)
 
-
-part2_df <- p2_merged_df6 %>%
-  mutate(year = "2016") %>%
-  mutate(rep_incumb = "0") %>%
-  mutate(pci_gro_sq = pci_gro^2) %>%
-  select(county.fips, year, county, state, rep.share, repshare.lag, unemp_gro,
-         pci_gro, pci_gro_sq, pop_thou, white.percent, rep_incumb, rural_percent)
-
-part2_df$year <- as.numeric(part2_df$year)
-part2_df$rep_incumb <- as.numeric(part2_df$rep_incumb)
-
-
-forecast_df <- bind_rows(part1_df, part2_df)
-
-
-f1 <- plm(rep.share ~ unemp_gro + repshare.lag, data = filter(forecast_df, year < 2016), model = 'within')
-f2 <- plm(rep.share ~ unemp_gro + repshare.lag + pop_thou + white.percent + as.factor(rep_incumb)
-          + unemp_gro:as.factor(rep_incumb) + rural_percent:white.percent, data = filter(forecast_df, year < 2016), model = 'within')
-
-stargazer::stargazer(f2, type = 'text', digits = 2, header = FALSE,   
-                     title = 'Model with Unemployment (OLS)', font.size = 'normalsize', out = 'fixed_part1.htm')
-
-test1 <- coeftest(f2, vcovHC(f2, method = "arellano"))
-
-stargazer::stargazer(test1, type = 'text', digits = 2, header = FALSE,   
-                     title = 'Model with Arellano', font.size = 'normalsize', out = 'fixed_part1_arellano.htm')
-
-
-#augment(f2, newdata = filter(forecast_df, year == 2016))
-#predict(f2, filter(forecast_df, year == 2016), se.fit = TRUE, interval = "confidence")
-#predict(f2, filter(forecast_df, year == 2016), se.fit = TRUE, interval = "prediction")
-
-
-
+part2_df <- p2_merged_df7 %>%
+  mutate(rep_incumb = 0) %>%
+  dplyr::select(county.fips, county, state, rep.share, repshare.lag, unemp_gro,
+         pci_gro, pop_thou, white.percent, rep_incumb, rural_percent, is.rep.2012)
 
 ######################################################Alternate####################################################
 
@@ -111,17 +76,25 @@ part2a_df <- part2a_df %>%
 
 png("Plot.png", width = 1800, height = 1800,
     res = 300)
-
-ggplot(part2a_df) +
-  geom_point(aes(x = pred_repshare, y = rep.share), alpha = 0.25) +
+part2a_df %>%
+  filter(!is.na(rep.share)) %>%
+ggplot() +
+  geom_point(aes(x = pred_repshare, y = rep.share, colour = as.factor(is.rep.2012)), alpha = 0.25) +
   geom_abline(slope = 1, intercept = 0) +
+  geom_segment(x = 0, y = 0.5, xend = 0.5, yend = 0.5, linetype = 2) +
+  geom_segment(x = 0.5, y = 0, xend = 0.5, yend = 0.5, linetype = 2) +
   labs(x = "Predicted Republican Voteshare", y = "Actual Republican Voteshare",
        title = "Residuals from 2016 US Presidential Elections Forecasting",
        caption = "Source: Dave Liep, US Election Atlas") +
-  theme_classic()
+  scale_colour_manual(values = c("steelblue", "red")) +
+  theme_classic() +
+  theme(
+    legend.position = "none"
+  )
 
 dev.off()
 
+p2_merged_df8$resid %>% abs() %>% mean(na.rm = T)
 ##################################################################################################################
 
 part2a_df2 <- part2a_df %>%
@@ -165,18 +138,6 @@ m25 <- lm(resid ~ -1 + manu_share_gro + av_wage_gro + lfpr_male_gro + gini_gro +
 
 #For rust belt states:
 m26 <- lm(resid ~ -1 + manu_share_gro + av_wage_gro + lfpr_male_gro + gini_gro + uneduc, p2_merged_df8_rust)
-
-
-
-m27 <- lm(resid ~ -1 + manu_share_gro + av_wage_gro + lfpr_male_gro + gini_gro + uneduc + lfpr_male_gro:uneduc, p2_merged_df8)
-
-#For swing states:
-m28 <- lm(resid ~ -1 + manu_share_gro + av_wage_gro + lfpr_male_gro + gini_gro + uneduc + lfpr_male_gro:uneduc, p2_merged_df8_swing)
-
-#For rust belt states:
-m29 <- lm(resid ~ -1 + manu_share_gro + av_wage_gro + lfpr_male_gro + gini_gro + uneduc + lfpr_male_gro:uneduc, p2_merged_df8_rust)
-
-#################################################################################################################
 
 
 
